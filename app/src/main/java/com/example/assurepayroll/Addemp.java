474c6550b28624625371423ff9d9fb8bdb2ab61f
mansivora8 <mansivora2001@gmail.com>
2021-02-03 13:45:20 +0530

    Add new employee, changing file name and password eye function

---
+++ app/src/main/java/com/example/assurepayroll/Addemp.java
@@ -0,0 +1,36 @@
+package com.example.assurepayroll;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Addemp extends Fragment {
+
+    private AddempViewModel mViewModel;
+
+    public static Addemp newInstance() {
+        return new Addemp();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.addemp_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(AddempViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/assurepayroll/AddempViewModel.java
@@ -0,0 +1,7 @@
+package com.example.assurepayroll;
+
+import androidx.lifecycle.ViewModel;
+
+public class AddempViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
