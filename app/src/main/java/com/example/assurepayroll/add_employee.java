f5332209d0676395dceec12e3cfbb309a8a38ddc
mansivora8 <mansivora2001@gmail.com>
2021-03-18 22:40:56 +0530

    error

--- app/src/main/java/com/example/assurepayroll/add_employee.java
+++ app/src/main/java/com/example/assurepayroll/add_employee.java
@@ -51,13 +51,11 @@ Button btnRegister;
 String URL="http://192.168.43.231:80/SDP_Payroll/register.php";
 //String URL="http://192.168.0.157:80/SDP_Payroll/register.php";
 
-//strings
 String name,email,dob,joiningDate,accNo,status,state,city,contact;
 
 
 static final String TAG = "Register";
 
-//spinner
 Spinner sp_state,sp_city;
 //arraylist state
 ArrayList<String> arrayList_state;
