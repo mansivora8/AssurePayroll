56fea2bfabdc65713513a057582fce00bba0d5ce
mansivora8 <mansivora2001@gmail.com>
2021-03-20 01:34:55 +0530

    add employee error resolved

--- app/src/main/java/com/example/assurepayroll/add_employee.java
+++ app/src/main/java/com/example/assurepayroll/add_employee.java
@@ -1,7 +1,6 @@
 package com.example.assurepayroll;
 
 import android.app.DatePickerDialog;
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,6 +16,8 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
